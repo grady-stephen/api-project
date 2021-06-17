230f929e6b78ae86c91f34cf2e4d0c45667018c3
stephen <stephen.h.nguyen.19@gmail.com>
2021-06-17 18:42:55 -0500

    adds edit btn

--- js/movies-api.js
+++ js/movies-api.js
@@ -36,7 +36,7 @@ function appendMovies(movie) {
 
 }
 function firstClick(movie){
-    $(`#movie-${movie.id}`).click(function (){
+    $(`#movie-${movie.id} *:first-child`).first().click(function (){
         $(`#movie-${movie.id}`).html(`<div class="movieDescription">
         <h1 class="display-6 text-center">${(movie.title).toUpperCase()}</h1>
         <h1 class="display-6 text-center">${movie.rating}/5 Stars</h1>
@@ -47,16 +47,22 @@ function firstClick(movie){
             <div class="my-4 mx-2 innerText"><strong>Directed by:</strong>${movie.director}</div>
             <div class="my-4 mx-2 innerText"><strong>Actors:</strong> ${movie.actors}</div>
             <div class="my-4 mx-2 innerText"><strong>Genres:</strong> ${movie.genre}</div>
-
+            
         </div>
-        </div>`)
+      
+        </div>`).append(`<div style="z-index: 1000; width: 100px; height: 100px">
+            <button id="edit-btn-${movie.id}" class="editBtn">Edit</button>
+</div>
+`)
         $(this).off("click");
+
         revertClick(movie);
+        addEditBtn()
 
     })
 }
 function revertClick(movie){
-    $(`#movie-${movie.id}`).click(function (){
+    $(`#movie-${movie.id} *:first-child`).first().click(function (){
 
         $(`#movie-${movie.id}`).html(`<img src=${movie.poster} class="moviePictures">`)
         $(this).off("click");
@@ -97,16 +103,17 @@ function postData(object){
     });
 }
 
-// (`<div class="movieDescription">
-//         <h1 class="display-6 text-center">${(movie.title).toUpperCase()}</h1>
-//         <h1 class="display-6 text-center">${movie.rating}/5 Stars</h1>
-//         <div class="border m-auto" style="width: 300px; height: 500px;">
-//             <div class="my-4 mx-2 innerText"> <strong>Released:</strong> ${movie.year} </div>
-//             <div class="my-4 mx-2 innerDescription"> <strong>Description:</strong> 1${movie.plot}
-//             </div>
-//             <div class="my-4 mx-2 innerText"><strong>Directed by:</strong>${movie.director}</div>
-//             <div class="my-4 mx-2 innerText"><strong>Actors:</strong> ${movie.actors}</div>
-//             <div class="my-4 mx-2 innerText"><strong>Genres:</strong> ${movie.genre}</div>
-//
-//         </div>
-//         </div>`)
+function scrollToTarget() {
+            $('html, body').animate({
+                scrollTop: $("#edit-container").offset().top
+            }, 2000);
+}
+
+
+    function addEditBtn (){
+        $('.editBtn').click(function (){
+            $('#form-container').show();
+            scrollToTarget()
+            console.log('success')
+        })
+    }
