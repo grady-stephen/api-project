1c49c7faba7ea85b8e69f29b0170417327c0980d
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-17 13:27:41 -0500

    appends each movie text

--- js/movies-api.js
+++ js/movies-api.js
@@ -1,6 +1,6 @@
 "use strict";
 $("#loading-pic").hide();
-
+//loads our database**
 function getMovies() {
     $("#loading-pic").show();
     $.ajax({
@@ -9,7 +9,7 @@ function getMovies() {
             data: {},
             success: function (data) {
                 console.log(data)
-                getPoster(data)
+                sortData(data)
             },
             complete: function () {
                 $("#loading-pic").hide();
@@ -21,15 +21,27 @@ function getMovies() {
 
 getMovies()
 
-function getPoster(data) {
+function sortData(data) {
     data.forEach(function (movie){
-        console.log(movie)
-        appenedPoster(movie.poster)
+        appendMovies(movie)
     })
 }
 
-function appenedPoster(url) {
-    $("#poster").append(`<img src=${url} class="moviePictures">`)
+function appendMovies(movie) {
+    $("#poster").append(`<div id="movie-${movie.id}"><img src=${movie.poster} class="moviePictures"></div>`);
+    $(`#movie-${movie.id}`).append(`<div class="movieDescription">
+        <h1 class="display-6 text-center">${(movie.title).toUpperCase()}</h1>
+        <h1 class="display-6 text-center">${movie.rating}/5 Stars</h1>
+        <div class="border m-auto" style="width: 300px; height: 500px;">
+            <div class="my-4 mx-2 innerText"> <strong>Released:</strong> ${movie.year} </div>
+            <div class="my-4 mx-2 innerDescription"> <strong>Description:</strong> 1${movie.plot}
+            </div>
+            <div class="my-4 mx-2 innerText"><strong>Directed by:</strong>${movie.director}</div>
+            <div class="my-4 mx-2 innerText"><strong>Actors:</strong> ${movie.actors}</div>
+            <div class="my-4 mx-2 innerText"><strong>Genres:</strong> ${movie.genre}</div>
+
+        </div>
+        </div>`)
 }
 
 
