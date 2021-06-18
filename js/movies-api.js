b365bd4b86819cf664ab46acd629d4e1334c1d4f
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-18 10:12:11 -0500

    editbuttons scroll

--- js/movies-api.js
+++ js/movies-api.js
@@ -1,8 +1,12 @@
 "use strict";
+
+
+
 $("#loading-pic").hide();
 //loads our database**
 function getMovies() {
     $("#loading-pic").show();
+    $("#poster").html("");
     $.ajax({
             url: 'https://capable-habitual-contraption.glitch.me/movies',
             type: "GET",
@@ -38,26 +42,29 @@ function appendMovies(movie) {
 function firstClick(movie){
     $(`#movie-${movie.id} *:first-child`).first().click(function (){
         $(`#movie-${movie.id}`).html(`<div class="movieDescription">
-        <h1 class="display-6 text-center">${(movie.title).toUpperCase()}</h1>
-        <h1 class="display-6 text-center">${movie.rating}/5 Stars</h1>
+        <h1 class="display-6 text-center movieTitle">${(movie.title).toUpperCase()}</h1>
+        <h1 class="display-6 text-center movieRating">${movie.rating}/5 Stars</h1>
         <div class="border m-auto" style="width: 300px; height: 500px;">
-            <div class="my-4 mx-2 innerText"> <strong>Released:</strong> ${movie.year} </div>
+            <div class="my-4 mx-2 innerText movieRelease"> <strong>Released:</strong> ${movie.year} </div>
             <div class="my-4 mx-2 innerDescription"> <strong>Description:</strong> ${movie.plot}
             </div>
-            <div class="my-4 mx-2 innerText"><strong>Directed by:</strong>${movie.director}</div>
-            <div class="my-4 mx-2 innerText"><strong>Actors:</strong> ${movie.actors}</div>
-            <div class="my-4 mx-2 innerText"><strong>Genres:</strong> ${movie.genre}</div>
+            <div class="my-4 mx-2 innerText movieDirected"><strong>Directed by:</strong>${movie.director}</div>
+            <div class="my-4 mx-2 innerText movieActors"><strong>Actors:</strong> ${movie.actors}</div>
+            <div class="my-4 mx-2 innerText movieGenre"><strong>Genres:</strong> ${movie.genre}</div>
             
         </div>
       
         </div>`).append(`<div style="z-index: 1000; width: 100px; height: 100px">
             <button id="edit-btn-${movie.id}" class="editBtn">Edit</button>
+            <button id="delete-btn-${movie.id}" class="deleteBtn">Delete</button>
 </div>
 `)
+        addEditBtn(movie);
+        addDeleteBtn();
         $(this).off("click");
 
         revertClick(movie);
-        addEditBtn()
+
 
     })
 }
@@ -104,16 +111,63 @@ function postData(object){
 }
 
 function scrollToTarget() {
-            $('html, body').animate({
-                scrollTop: $("#edit-container").offset().top
-            }, 2000);
+    $("html").animate(
+        {
+            scrollTop: $("#form-container").offset().top
+        },
+        1200 //speed
+    );
 }
 
 
-    function addEditBtn (){
+function putData(movie){
+    $("#submitForm").attr('id', "putBtn");
+    let title = $("#titleInput").val( `${movie.title.toUpperCase()}`);
+    let rating = $("#ratingInput").find(`#value${movie.rating}`).attr('selected', "selected");
+    let actors = $("#actors-input").val( `${movie.actors}`);
+    let director = $("#directorInput").val( `${movie.director}`);
+    let plot = $("#plotInput").val( `${movie.plot}`);
+    let year = $("#releaseInput").val( `${movie.year}`);
+   let genre = $("#genreInput").val( `${movie.genre}`);
+
+$("#putBtn").click(function(){
+    $.ajax({
+        url: `https://capable-habitual-contraption.glitch.me/movies/${movie.id}`,
+        type: "PUT",
+        data: {
+            title: title,
+            rating: rating,
+            actors: actors,
+            director: director,
+            plot: plot,
+            year: year,
+            genre: genre,
+        },
+        success: function () {
+            console.log("PUT");
+            getMovies();
+        }
+    })
+})
+
+}
+
+
+
+    function addDeleteBtn(){
+        $('.deleteBtn').click(function (){
+            $('#form-container').show();
+            scrollToTarget();
+
+        })
+
+    }
+    function addEditBtn (movie){
         $('.editBtn').click(function (){
             $('#form-container').show();
-            scrollToTarget()
-            console.log('success')
+            scrollToTarget();
+            $("#formTitle").text("Edit Movie:");
+            console.log(movie.id);
+            putData(movie);
         })
     }
